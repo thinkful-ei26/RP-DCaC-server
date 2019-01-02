bb696859714569ade6816ef23b057317b2d0328f
RP Boyle <rpboyle11@yahoo.com>
2019-01-02 15:41:07 -0500

    organizing config.js

--- config.js
+++ config.js
@@ -2,8 +2,6 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3001',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/donna-crafts',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/crafts-test'
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  MONGODB_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/donna-crafts'
 };
